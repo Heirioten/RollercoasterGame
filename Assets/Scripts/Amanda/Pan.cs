767b17ec1be2215bc4b654d2f4d087c48d78116d
Amanda Keith <akeith@clarku.edu>
2025-11-11 19:01:00 -0500

    Zoom&Pan

---
+++ Assets/Scripts/Amanda/Pan.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3aa63732587ca48ea3ad197c89b3c385637cf2b83c154ba02a06a29708d33dd8
+size 1377
---
+++ Assets/Scripts/Amanda/Zoom.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f4d8be981f36ed40df399e161d207d9501f7228374cd151cafd65c2cb18209b
+size 865
