00673dc9f3022945d79a5cdbec4f9d9476e0404f
Nicholas <nicholasjohnlyons@gmail.com>
2025-11-11 19:06:19 -0500

    Track Placement with Curves

--- Assets/Scripts/Nicky/MySpline.cs
+++ Assets/Scripts/Nicky/MySpline.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f2ecad99e0fee7d754021b8ea49cc7aacbb6b715baa9e8f38f6bd4080f817ed
-size 10700
+oid sha256:fdfc869b72c9a50b07a9a94d681ecfa790b04f8edd368c84d2e16bab0f27608b
+size 11303
