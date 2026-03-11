00d1ad5db579fb5191b06da3222d69f9d67408a2
Amanda Keith <akeith@clarku.edu>
2026-03-11 11:31:34 -0400

    LevelPreset

--- Assets/Scripts/Project/Level_System/LevelFactory.cs
+++ Assets/Scripts/Project/Level_System/LevelFactory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8413e99d094d88f5c2f3280cead61045d18d9a3062e98571fcee05a852e15dc3
-size 500
+oid sha256:0edb9fafd831538c0e5baa3f84311116a6c14290660990ead80da74e0e760420
+size 625
---
+++ Assets/Scripts/Project/Level_System/LevelPreset.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba32d1d62c2fa24c0ff378aad6a5cba9a767cf81015ab417e3d54a629d8fb49e
+size 766
