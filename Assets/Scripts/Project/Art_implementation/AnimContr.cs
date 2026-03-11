db2c4f8ae3c62b45300cb1c01133a687b3b89a49
Amanda Keith <akeith@clarku.edu>
2026-03-10 21:59:53 -0400

    Background/LevelClass

---
+++ Assets/Scripts/Project/Art_implementation/AnimContr.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f190d2c0fd0baf92171edddbbe6cfea8a5bdf03e9a3f428739bc40aa4fcff8f7
+size 210
---
+++ Assets/Scripts/Project/Level_System/LevelData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f43bcb9da075b69b39a38552cbe736c6e3d2de772aaf6ec62d04135f2c531f8
+size 686
