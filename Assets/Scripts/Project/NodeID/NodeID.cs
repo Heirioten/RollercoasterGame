c4da0398b69767925534f7099e62aaa948a75abd
Nicholas <nicholasjohnlyons@gmail.com>
2026-03-11 12:17:22 -0400

    Rename disappearing NodeData to NodeID

---
+++ Assets/Scripts/Project/NodeID/NodeID.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36cb3eb4b4dc4e907c4e1166ac409fca6ad09a13718dcf2b80b318213b328a78
+size 217
--- Assets/Scripts/Project/Track_System/Spline_Components/SplineRenderer.cs
+++ Assets/Scripts/Project/Track_System/Spline_Components/SplineRenderer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a135778aa9ff02926f98b6d6d1d0f551dd0b323823e5c860c0969b186cf288b8
-size 3811
+oid sha256:e35a8caf1d6296a3c74682d1b240d04ac5c8ac3c2ca71e5c3d4ba45d3b8182ed
+size 3809
